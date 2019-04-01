9b5854854ff976234da1f2f97a782589003eaec7
AmaroPy <julianamaro@gmail.com>
2019-04-01 13:12:58 +0200

    Update with Study drills

--- 04lesson.py
+++ 04lesson.py
@@ -1,10 +1,25 @@
+# Cuántos coches hay
 cars = 100
-space_in_a_car = 4.0
+
+# Cuánto espacio hay en los coches
+space_in_a_car = 4.0 # Si cambias el numero decimal por un número no_decimal, sigue funcionando.
+
+# Cuántos conductores hay
 drivers = 30
+
+# Cuántos pasajeros hay
 passengers = 90
+
+# Cuántos coches no tienen conductor
 cars_not_driven = cars - drivers
+
+# Cuántos coches SI tienen conductor
 cars_driven = drivers
+
+# Cuántos huecos tenemeos en total con los coches que SI tienen conductor
 carpool_capacity = cars_driven * space_in_a_car
+
+# Cuántos pasajeros podemos llevar en el día de hoy
 average_passengers_per_car = passengers / cars_driven
 
 
