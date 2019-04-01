68078aeae443adaf8f0472f80a8d3523f52ad0d8
AmaroPy <julianamaro@gmail.com>
2019-04-01 12:44:33 +0200

    update with Study drills

--- 03lesson.py
+++ 03lesson.py
@@ -1,23 +1,40 @@
+# That line print the title
 print("I will now count my chickens:")
 
+# That line print how many hens I have
 print("Hens", 25 + 30 / 6)
+# That line print how many roosters I have
 print("roosters", 100 - 25 * 3 % 4)
 
+# that line print another title
 print("Now i will count the eggs:")
 
+# that line use math to calculate how many eggs I have
 print(3 + 2 +1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# that line print an example using math into a string
 print("Is it true that 3 + 2 < 5 -7?")
 
+# that line print an example using numbers
 print(3 + 2 < 5 - 7)
 
+# that line print and example using string and numbers
 print("What is 3 + 2?", 3 + 2)
+
+# that line print another example using string and numbers
 print("What is 5 - 7?", 5 - 7)
 
+# that line print the result for the example
 print("Oh, that's why it's False.")
 
+# that line print another title
 print("How about some more.")
 
+# that line print another example using string and numbers
 print("Is it greater?", 5 >= -2)
+
+# that line print another example using string and numbers
 print("Is it greater or equal?", 5 >= -2)
+
+# that line print another example using string and numbers
 print("Is it less or equal?", 5 <= -2)
