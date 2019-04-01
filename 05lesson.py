ede84abe818b15fc825b0b7f1dd55931ab08bf60
AmaroPy <julianamaro@gmail.com>
2019-04-01 12:31:31 +0200

    updating with Study drills

--- 05lesson.py
+++ 05lesson.py
@@ -1,18 +1,22 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+inches_height = 74 # inches
+cent_height = inches_height * 2.54
+lbs_weight = 180 #lbs
+kilo_weight = lbs_weight * 0.453592
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {inches_height} inches tall.")
+print(f"He's {round(cent_height)} centimeters tall.")
+print(f"He's {lbs_weight} pounds heavy.")
+print(f"He's {round(kilo_weight)} kilograms heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes}eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes}eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If i add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + cent_height + kilo_weight
+print(f"If i add {age}, {cent_height}, and {kilo_weight} I get {total}.")
