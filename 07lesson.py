8597bce127e7725e5fb04807871d7175a06d68ce
AmaroPy <julianamaro@gmail.com>
2019-04-01 17:45:31 +0200

    update with Study drills

---
+++ 07lesson.py
@@ -0,0 +1,28 @@
+# Esto constituye la primera parte de la canción
+print("Mary had a little lamb.")
+print("Its fleece was white as {} .".format('snow'))
+print("And everywhere that Mary went.")
+
+# Con este print hacemos que aparezcan 10 puntos y separen de la respuesta
+print("." * 10) # what'd that do?
+
+# Hemos nombrado cada una de las variables para luego concatenarlas en los print sucesivos
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Hemos concatenado todas las variables para formar la respuesta
+#watch end = ' ' at the end. Try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+
+# Al introducir en la linea anterior "end = ' '" en lugar de hacer una bajada de linea, la respuesta se escribe seguida
+print(end7 + end8 + end9 + end10+ end11 + end12)
