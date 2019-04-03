c57c5b68ad0e108484c9a519e4298480a0a37879
AmaroPy <julianamaro@gmail.com>
2019-04-03 10:56:01 +0200

    lesson 11 hard way

---
+++ 11lesson.py
@@ -0,0 +1,12 @@
+print("How old are you"),
+age = input()
+print("How tall are you"),
+height = input()
+print("How much do you weigh?"),
+weight = input()
+
+# Study drills
+print("How are you?")
+hi = input()
+
+print("So, you're {} old, {} tall and {} heavy.".format(age, height, weight))
