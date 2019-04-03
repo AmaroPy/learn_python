20f58aa83e3b4ff6117aab6f83a6d113eb8eb585
AmaroPy <julianamaro@gmail.com>
2019-04-03 10:56:16 +0200

    inputs, hard way

---
+++ 12lesson.py
@@ -0,0 +1,7 @@
+y = input("Name? ")
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do yo weigh? ")
+
+print("So, you're {} old, {} tall, {} heavy.".format(age, height, weight))
