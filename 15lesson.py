ab1a862bac94eb68cc722a5095af40713ce12976
AmaroPy <julianamaro@gmail.com>
2019-04-06 22:48:48 +0200

    update lesson 15 hard way

--- 15lesson.py
+++ 15lesson.py
@@ -4,5 +4,5 @@ script, filename = argv
 
 # with open es BIEN
 with open(filename, 'r') as document:
-    print("Here's your file".format(filename))
+    print("Here's your file {}".format(filename))
     print(document.read())
