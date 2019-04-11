457b9da6d67ba2bdc0500a7b91d20839afb5915d
AmaroPy <julianamaro@gmail.com>
2019-04-11 18:02:25 +0200

    lesson 34 hard way

---
+++ 34lesson.py
@@ -0,0 +1,30 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+'''
+1. The animal at 1 is the 2nd animal and is a python.
+2. The 3rd animal is at 2 and is a peacock.
+3. The 1st animal is at 0 and is a bear.
+4. The animal at 3 is the 4th and is a kangaroo.
+5. The 5th animal is at 4 and is a whale.
+6. The animal at 2 is the 3rd and is a peacock.
+7. The 6th animal is at 5 and is platypus.
+8. The animal at 4 is the 5th and is whale
+'''
+
+new_animals = ['python', 'peacock', 'bear', 'kangaroo', 'whale', 'peacock', 'platypus', 'whale']
+
+'----------------------------------------------------------------'
+''' # Study drills
+1. The study drill at 1 is the 2nd and is azul.
+2. The 3rd study drill is at 2 and is naranja.
+3. The 1st study drill is at 0 and is rojo.
+4. The study drill at 3 is the 4th and is rosa.
+5. The 5th study drill is at 4 and is blanco.
+6. The study drill at 2 is the 3rd and is naraja.
+7. The 6th study drill is at 5 and is negro.
+8. The study drill at 4 is the 5th and is blanco.
+'''
+study_drills = ['rojo', 'azul', 'naranja', 'rosa', 'blanco', 'negro']
