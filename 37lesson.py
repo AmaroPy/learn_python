7f16ebeb8434913f6531da9245a3d6840223749c
AmaroPy <julianamaro@gmail.com>
2019-04-17 01:23:46 +0200

    updated 37lesson hard way

---
+++ 37lesson.py
@@ -0,0 +1,104 @@
+# Repaso de palabras y símbolos usados en python
+# Keywords
+and # resquisito que necesita que dos parametros se cumplan a la vez
+del # comando para borrar
+from # comando para "desde" refiriendose a otro archivo no ejecutado
+not # invierte el booleano o condición a la que acompaña
+while # para bucles
+as # 
+elif # fija otra condición si no se cumple la anterior
+global
+or # requisito que necesita que dosparametros se cumpla uno u otro
+with
+assert
+else # fija una condición cuando ninguna otra condición se cumple
+if # fija la primera condición
+pass # salta la lectura de código
+yield
+break # hace parar el bucle y salir de él
+except
+import # importa otras funciones de otros archivos
+print # imprime por pantalla
+class
+exec
+in
+raise
+continue
+finally
+is
+return # para devolver resultados de funciones
+def # para definir funciones
+for # bucle mas usado, para recorrer o pasar por cada parámetro
+lambda
+try
+
+# Data types
+True # booleano
+False # booleano
+None # Objeto pero es NADA
+strings # tipo de parametro texto "ésto es texto"
+numbers # tipo de parametro número entero 5, 6, 7
+floats # tipo de parametro número con decimales 5.6, 6.5, 7.6
+lists # lista de parametros de cualquier tipo [ésto, 5, es, 6, una, 7, lista]
+
+#String escapes sequences
+\\ # añade una Barra inclinada \ al string
+\' # añade ' una comilla, como cuando quieres hacer incapié en algo
+\" # Añade dobles comillas para hacer incapié en algo
+\a # Sonido, timbre, aleta sonora
+\b # borra la letra o "espacio + letra " al que acompaña
+\f # Añade salto de linea e indenta el string
+\n # salto de línea
+\r # Mueve todo lo que hay despues de \r hasta del string
+\t # Añade una tabulación horizontal
+\v # Añade un salto de línea y lo ajusta en formato a la continuación de donde acaba el string
+
+# String formats # ésta es la manera antigua.
+%d # 
+%i
+%o
+%u
+%x 
+%X 
+%e 
+%E 
+%f 
+%F 
+%g 
+%G 
+%c
+%r 
+%s 
+%%
+
+# Operators
++ 
+-
+*
+**
+/
+//
+%
+<
+>
+<=
+>=
+==
+!=
+<>
+( )
+[ ]
+{ }
+@
+,
+:
+.
+=
+;
++=
+-=
+*=
+/=
+//=
+%=
+**=
